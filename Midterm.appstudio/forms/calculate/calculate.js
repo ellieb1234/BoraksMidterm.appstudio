4b98ce8bc9156977b493e1f20af98115996694de
ellieb1234 <ellieboraks@creighton.edu>
2021-03-15 16:58:33 -0500

    calculate

---
+++ Midterm.appstudio/forms/calculate/calculate.js
@@ -0,0 +1,13 @@
+let a=parseInt(prompt("Enter three numbers"))
+let b=parseInt(prompt("Enter another number"))
+let c=parseInt(prompt("Enter another number"))
+
+let avgSquare=calcAvgSquare(a,b,c)
+console.log(`The answer is ${avgSquare}.`)
+
+function calcAvgSquare(x,y,z){
+  let xSquare=x**2
+  let xAvg=(x+y+z)/3
+  let avgXSquare=xAvg*xSquare
+  return (avgXSquare)
+}
