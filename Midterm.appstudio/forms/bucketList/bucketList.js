a085e685df6a8bb5dd5ef15f3af3723646f5fcb1
ellieb1234 <ellieboraks@creighton.edu>
2021-03-15 17:22:37 -0500

    Update bucketList.js

--- Midterm.appstudio/forms/bucketList/bucketList.js
+++ Midterm.appstudio/forms/bucketList/bucketList.js
@@ -1,6 +1,7 @@
 //Whenever I run this code I get the following error: TypeError: undefined is not an object (evaluating 'bucketList.style.display = 'block'')
 //I have no idea what this means nor how to fix it.  Other than this error, my code runs properly and logs the correct output
 
+/*
 let bucketList=[]
 let newItem=prompt("Add an item to the bucket list.  To see the bucket list type 'LIST'.  To end the program type 'STOP'.")
 
@@ -15,3 +16,4 @@ while (newItem!='STOP') {
   }
   newItem=prompt("Add an item to the bucket list.  To see the bucket list type 'LIST'.  To end the program type 'STOP'.")
 }
+*/
