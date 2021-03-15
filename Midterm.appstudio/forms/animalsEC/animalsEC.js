83f9ce22244de56a0e3c16435512b98f9c247c7c
ellieb1234 <ellieboraks@creighton.edu>
2021-03-15 16:06:04 -0500

    animalsEC

--- Midterm.appstudio/forms/animalsEC/animalsEC.js
+++ Midterm.appstudio/forms/animalsEC/animalsEC.js
@@ -1,10 +1,10 @@
-let myAnimals = ['dog', 'cat', 'horse', 'meerkat']
-let newAnimal = prompt("Type an animal or 'STOP' to end the program")
+myAnimals = ['dog', 'cat', 'horse', 'meerkat']
+newAnimal = prompt("Type an animal or 'STOP' to end the program")
 
 while (newAnimal!='STOP'){
   myAnimals.push(newAnimal.toLowerCase())
   newAnimal = prompt("Type an animal or 'STOP' to end the program")
 }
 
-let lastAnimal=myAnimals[myAnimals.length-1]
+lastAnimal=myAnimals[myAnimals.length-1]
 console.log(`The last animal is a/an ${lastAnimal}.`)
